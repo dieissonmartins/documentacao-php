cc65241acbcab16dbb4cd5170e7f230eee6f724b
Dieisson Martins <dieisson.martins.santos@gmail.com>
2018-08-26 13:07:22 -0300

    estruturas de controles

---
+++ estruturas-de-controle.php
@@ -0,0 +1,21 @@
+<?php 
+//bloco
+//*********************************************************
+
+$vai_chover = FALSE;
+$esta_frio	= TRUE;
+
+
+	if ($vai_chover == TRUE and $esta_frio == TRUE) {
+		
+		echo"Não vou sair de casa";
+
+	}elseif ($vai_chover == FALSE and $esta_frio == TRUE) {
+		echo"Vou sair de casa";
+	}
+
+//*********************************************************
+
+
+
+?>
