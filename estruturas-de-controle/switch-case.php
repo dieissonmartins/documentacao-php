ff615c38b031c41997704762ba5c7b27ad8d950a
Dieisson Martins <dieisson.martins.santos@gmail.com>
2018-09-20 21:13:57 -0300

    add estrutura de controle Switch Case

---
+++ estruturas-de-controle/switch-case.php
@@ -0,0 +1,24 @@
+<?php 
+
+$i = 2;
+
+switch ($i) {
+	case '1':
+		echo" I é igual a 1";
+	break;
+	case '2':
+		echo" I é igual a 2";
+	break;
+	case '3':
+		echo" I é igual a 3";
+	break;
+	case '4':
+		echo" I é igual a 4";
+	break;
+	
+	default:
+		echo " I é igual a um numero qualquer";
+	break;
+}
+
+?>
