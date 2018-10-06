c2d2375f0fc771127fa1a0d6b39f1a38c9a54016
Dieisson Martins <dieisson.martins.santos@gmail.com>
2018-10-06 12:50:50 -0300

    tipos de strings

--- manipulacao-strings/mani-1/index.php
+++ manipulacao-strings/mani-1/index.php
@@ -1,9 +1,23 @@
 <?php
 
+//string normal 
 $nome = "Dieisson";
 $sobrenome = "Martins dos Santos";
 
 echo "Meu nome é {$nome} {$sobrenome} </br>";
+
+
+//string quebrada
+$texto = <<<CHAVE
+	linha1
+		linha2
+			linha3
+				linha4
+CHAVE;
+
+echo $texto;
+
+
 
 
 
