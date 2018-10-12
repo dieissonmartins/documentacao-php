ac340f6d01b81d627f254f777b2b89ad341a3ae7
Dieisson Martins <dieisson.martins.santos@gmail.com>
2018-10-12 12:06:30 -0300

    add metodos getres e setres da classe produto

--- programacao-orientada-objeto/objeto3.php
+++ programacao-orientada-objeto/objeto3.php
@@ -5,13 +5,37 @@ class Produto{
 	private $descricao;
 	private $estoque;
 	private $preco;
+
+	function setDescricao($descricao){
+		if ( is_string($descricao)){
+			$this->descricao = $descricao;
+		}
+	}
+
+	function getDescricao(){
+		return $this->descricao;
+	}
+
+	function setEstoque($estoque){
+		if(is_numeric($estoque) and $estoque >0 ){
+			$this->estoque = $estoque;
+		}
+	}
+
+	public function getEstoque(){
+		return $this->estoque;
+	}
+
 }
 
-$produto1 = new Produto;
+	$produto1 = new Produto;
+	$produto1->setDescricao('cafe');
+	$produto1->setEstoque(10);
+	
+		echo 'Descrição:'.$produto1->getDescricao().'</br>';
+		echo 'Estoque:'.$produto1->getEstoque().'</br>';
 
-$produto->descricao = "cafe";
-$produto->estoque   = 123;
-$produto->preco     = 12.43;
+
 
 
 
