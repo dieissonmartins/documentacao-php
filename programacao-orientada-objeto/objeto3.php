f6234073f85d49b62fba88815347cdbd826c1a5a
Dieisson Martins <dieisson.martins.santos@gmail.com>
2018-10-12 11:49:03 -0300

    classe produto com atributos privados

---
+++ programacao-orientada-objeto/objeto3.php
@@ -0,0 +1,18 @@
+<?php
+
+class Produto{
+
+	private $descricao;
+	private $estoque;
+	private $preco;
+}
+
+$produto1 = new Produto;
+
+$produto->descricao = "cafe";
+$produto->estoque   = 123;
+$produto->preco     = 12.43;
+
+
+
+?>
