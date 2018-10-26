96714564677c65c71eb4d3b3bad18944da1dfde2
Dieisson Martins <dieisson.martins.santos@gmail.com>
2018-10-26 20:29:57 -0300

    Objetos associativos

---
+++ programacao-orientada-objeto/objeto-7-associcao-de-arquivos/associacao.php
@@ -0,0 +1,16 @@
+<?php
+
+//chamadas das classes
+require_once'class/Fabricante.php';
+require_once'class/Produto.php';
+
+//criação dos Objetos
+$produto1 = new Produto('Chocolate',12,6);
+$fabricante1 = new Fabricante('Chocolate Factory','Casa Martins','7771'); 
+
+//associacao
+$produto1->setFabricante( $f1 );
+
+echo 'Descricao '.$produto1->setDescricao()." </br>";
+echo 'Fabricante '.$produto1->setFabricante()->getNome()." </br>";
+?>
