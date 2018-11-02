039eb3833c527e5eace01afba1c4128351b7581a
Dieisson Martins <dieisson.martins.santos@gmail.com>
2018-11-02 20:30:34 -0300

    pagina 104

--- programacao-orientada-objeto/objeto-7-associcao-de-arquivos/associacao.php
+++ programacao-orientada-objeto/objeto-7-associcao-de-arquivos/associacao.php
@@ -9,8 +9,8 @@ $produto1 = new Produto('Chocolate',12,6);
 $fabricante1 = new Fabricante('Chocolate Factory','Casa Martins','7771'); 
 
 //associacao
-$produto1->setFabricante( $f1 );
+$produto1->setFabricante( $fabricante1 );
 
-echo 'Descricao '.$produto1->setDescricao()." </br>";
+echo 'Descricao '.$produto1->getDescricao()." </br>";
 echo 'Fabricante '.$produto1->setFabricante()->getNome()." </br>";
 ?>
