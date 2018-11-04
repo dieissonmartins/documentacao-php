ef158d4d3d397ce973dbb5d97d1a8ce7d43e9881
Dieisson Martins <dieisson.martins.santos@gmail.com>
2018-11-03 21:00:41 -0300

    correcao nas classes de herança no arquivo de polimorfismo

--- programacao-orientada-objeto/objeto-9-heranca/polimorfismo.php
+++ programacao-orientada-objeto/objeto-9-heranca/polimorfismo.php
@@ -12,7 +12,17 @@ $contas[] = new ContaPoupanca(3333,"WW.4323.75",100);
 
 foreach ($contas as $key => $conta){
 	echo "Conta {$conta->getInfo()} </br>";
-	echo "Saldo {$conta->getInfo()} </br>";
+	echo "Saldo {$conta->getSaldo()} </br>";
+
+	$conta->depositar(332);
+	echo "Desposito de 200 </br>";
+	echo "Saldo Atual: {$conta->getSaldo()} </br>";
+	if($conta->retirar(700)){
+		echo "retirada de 700 </br>";
+	}else{
+		echo "retirada de 700 não permitida </br>";
+	}
+	echo "Saldo Atual: {$conta->getSaldo()} </br>";
 }
 
 
