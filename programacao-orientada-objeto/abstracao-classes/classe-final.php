d050b8210365f7a76aa8647d504c1443e7b27665
Dieisson Martins <dieisson.martins.santos@gmail.com>
2018-12-12 20:33:41 -0200

    classe final conta universitaria

---
+++ programacao-orientada-objeto/abstracao-classes/classe-final.php
@@ -0,0 +1,10 @@
+<?php
+
+require_once('classes/Conta.php');
+require_once('classes/contaPoupanca.php');
+
+class ContaPoupancaUniversitaria extends ContaPoupanca{
+	//..
+}
+
+?>
---
+++ programacao-orientada-objeto/abstracao-classes/classes/contaPoupanca.php
@@ -0,0 +1,5 @@
+<?php
+	final class ContaPoupanca extends Conta{
+		
+	}
+?>
