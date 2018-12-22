1ffc877b2645a4577cdb8d688d21da71df0e144c
Dieisson Martins <dieisson.martins.santos@gmail.com>
2018-12-22 11:15:22 -0200

    funcao retorna vetor com nomes dos metodos de uma classe

---
+++ programacao-orientada-objeto/funcoes_manipulacao_objetos/get_class_methods.php
@@ -0,0 +1,23 @@
+<?php
+
+//classe para objetos
+class Funcionario{
+
+	//metodos da classe
+	function setSalario(){
+		//pegar valor usando SET
+	}
+	function getSalario(){
+		//mostrar valor usando GET
+	}
+	function setNome(){
+		//pegar valor usando SET
+	}
+	function getNome(){
+		//mostrar valor usando GET
+	}
+}
+
+print_r(get_class_methods('Funcionario'));
+
+?>
