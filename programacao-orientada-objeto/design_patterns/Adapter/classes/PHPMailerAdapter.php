4fc4967637b47f8fad79e6c6a27761d8f0db9f29
Dieisson Martins <dieisson.martins.santos@gmail.com>
2018-12-29 17:54:17 -0200

    feita classe PHPMailerAdapter do design pattern ::Adapter:: usando PHPMailer

---
+++ programacao-orientada-objeto/design_patterns/Adapter/classes/PHPMailerAdapter.php
@@ -0,0 +1,69 @@
+<?php
+
+class PHPMailerAdapter{
+	private $pm;
+
+	public function __construct(){
+		$this->pm = new PHPMailer;
+		$this->pm->CharSet = 'utf-8';
+	}
+
+	public function setDebug($bool){
+		$this->pm->SMTPDebug = $bool; 
+	}
+
+	public function setFrom($from,$name){
+		$this->pm->From = $from;
+		$this->pm->FromName = $name;
+	}
+
+	public function setSubject($subject){
+		$this->pm->Subject = $subject;
+	}
+
+	public function setTextBody($body){
+		$this->pm->Body = $body;
+		$this->pm->IsHTML(FALSE);
+	}
+
+	public function setHTMLBody($html){
+		$this->pm->MsgHTML($html);
+	}
+
+	public function addAddress($address,$name = ''){
+		$this->pm->AddAddress($address,$name);
+	} 
+
+	public function addAttach($path,$name=''){
+		$this->pm->addAttachment($path,$name);
+	}
+
+	public function setUseSmtp($auth = TRUE){
+		$this->pm->IsSMTP();
+		$this->pm->SMTPAuth = $auth;
+	}
+
+	public function  setSmtpHost($host, $port = 25){
+		$this->pm->Host = $host;
+		$this->pm->Port = $port;
+		$this->pm->SMTPSecure = "ssl";
+	}
+
+	public function setSmtpUser($user, $pass){
+		$this->pm->Username = $user;
+		$this->pm->Password = $pass;
+	}
+
+	public function send(){
+		if (!$this->pm-> Send()) {
+			if ($this->pm->SMTPDebug) {
+				throw new Exception('E-mail not sent: ', $this->pm->ErrorInfo);
+			}else{
+				throw new Exception('E-mail not sent: ');
+			}
+		}
+		return TRUE;
+	}
+}
+
+?>
