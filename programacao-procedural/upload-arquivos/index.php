8e9fcb7447afade248e0f59c48eda1b4e29827c8
dieisson <dieisson.martins.santos@gmail.com>
2019-01-31 16:01:24 -0200

    upload de arquivos de imagens

---
+++ programacao-procedural/upload-arquivos/index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title>Importar arquivos</title>
+</head>
+<body>
+	<header id="cabecalho">
+		
+	</header>
+	<main id="principal">
+		<section>
+			<form action="upload.php" method="post" enctype="multipart/form-data">
+			    Selecione o arquivo para upload:
+			    <input type="file" name="fileToUpload" id="fileToUpload">
+			    <input type="submit" value="Upload Image" name="submit">
+			</form>
+		</section>
+	</main>
+	<footer id="rodape">
+		
+	</footer>
+</body>
+</html>
---
+++ programacao-procedural/upload-arquivos/upload.php
@@ -0,0 +1,59 @@
+
+
+<?php
+
+$diretorio = "uploads/";
+echo $diretorio_arquivo = $diretorio . basename($_FILES["fileToUpload"]["name"]);
+$uploadOk = 1;
+$imageFileType = strtolower(pathinfo($diretorio_arquivo,PATHINFO_EXTENSION));
+
+// Verifique se o arquivo de imagem é uma imagem real ou uma imagem falsa
+if(isset($_POST["submit"])) {
+ 
+    $check = getimagesize($_FILES["fileToUpload"]["tmp_name"]);
+ 
+    if($check !== false) {
+        echo "Arquivo é uma imagem - " . $check["mime"] . ".";
+        $uploadOk = 1;
+    } else {
+        echo "Arquivo não é uma imagem.";
+        $uploadOk = 0;
+    }
+}
+
+// Verifica se o arquivo já existe
+if (file_exists($diretorio_arquivo)) {
+    
+    echo "Desculpe, o arquivo já existe.";
+    $uploadOk = 0;
+}
+
+// Verifique o tamanho do arquivo
+if ($_FILES["fileToUpload"]["size"] > 500000) {
+
+    echo "Desculpe, seu arquivo é muito grande.";
+    $uploadOk = 0;
+}
+
+// Permitir determinados formatos de arquivo
+if($imageFileType != "jpg" && $imageFileType != "png" && $imageFileType != "jpeg" && $imageFileType != "gif" ) {
+    
+    echo "Desculpe, somente arquivos JPG, JPEG, PNG e GIF são permitidos.";
+    $uploadOk = 0;
+}
+
+// Verifique se $uploadOk está definido como 0 por um erro
+if ($uploadOk == 0) {
+    
+    echo "Desculpe, seu arquivo não foi enviado.";
+}
+
+// se tudo estiver ok, tente fazer o upload do arquivo
+else {
+    if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $diretorio_arquivo)) {
+        echo "O Arquivo ". basename( $_FILES["fileToUpload"]["name"]). "foi enviado...";
+    } else {
+        echo "Desculpe, houve um erro ao enviar seu arquivo.";
+    }
+}
+?>
