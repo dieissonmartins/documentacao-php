c854ff1ebf7ca31a3c018344db695b8f02502b69
Dieisson Martins <dieisson.martins.santos@gmail.com>
2019-02-01 21:35:09 -0200

    crud nivel 6

---
+++ poo_estruturado_database/nivel_6_conexoes_seguranca/pessoa_list.php
@@ -0,0 +1,41 @@
+<?php
+
+require_once('classes/Pessoa.php');
+
+try{
+	if (!empty($_GET['action']) AND $_GET['action'] =='delete') {
+		$id = (int)$_GET['id'];
+		Pessoa::delete($id);
+	}
+}
+
+$pessoas = Pessoa::all();
+
+var_dump($pessoas);
+
+catch(Exception $e){
+	print $e->getMessage();
+}
+
+$items = '';
+
+if ($pessoas){
+	foreach ($pessoas as $pessoa){
+		
+		$item = file_get_contents('html/item.html');
+		$item = str_replace('{id}', $pessoa['id'], $item);
+		$item = str_replace('{nome}', $pessoa['nome'], $item);
+		$item = str_replace('{endereco}', $pessoa['endereco'], $item);
+		$item = str_replace('{bairro}', $pessoa['bairro'], $item);
+		$item = str_replace('{telefone}', $pessoa['telefone'], $item);
+
+		$items .= $item;
+	}
+}
+
+$list = file_get_contents('html/list.html');
+$list = str_replace('{items}', $items, $list);
+
+echo $list;
+
+?>
