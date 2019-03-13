9ca9f40fbd2e85872745b8d73869ce4e9fc1c67d
dieisson <dieisson.martins.santos@gmail.com>
2019-03-13 09:23:44 -0300

    validate email

---
+++ validacao-dados-formulario/email/index.php
@@ -0,0 +1,13 @@
+<?php
+
+$email = "";
+
+
+if(!preg_match("^[_a-z0-9-]+(\.[_a-z0-9-]+)*@[a-z0-9-]+(\.[a-z0-9-]+)*(\.[a-z]{2,3})$^",$email)){ 
+
+	echo "<center><font face='Verdana' size='2' color=red>Email não é Valido</font></center>";
+}else{
+	echo "<center><font face='Verdana' size='2' color=green>Email é Valido</font></center>";
+}
+
+?>
